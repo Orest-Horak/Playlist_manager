ef1ed1808307c7663443256a476bef2fc4669210
MykolaPMP <31858361+MykolaPMP@users.noreply.github.com>
2017-10-05 21:26:35 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -9,7 +9,11 @@ void ls(string directory);
 
 void main()
 {
+	/*Playlist A;*/
+
+
 	string directory= "c:\\";
+	
 	string operation="";
 	do 
 	{
@@ -19,8 +23,61 @@ void main()
 		{
 			ls(directory);
 		}
-		if (operation.find("exit") == 0)
+		else if (operation.find("cd") == 0) 
 		{
+			if (operation.find("..") == 3) 
+			{
+				if (directory.length() <= 4) 
+				{
+					cout << "Operations can't be performed" << endl;
+				}
+				else 
+				{
+					directory.erase((directory.substr(0,directory.length()-2)).rfind("\\")+1);
+				}
+			}
+			else if (operation.find("\\") == operation.length() - 2) 
+			{
+				directory += operation.substr(3, operation.length() - 2);
+			}
+			else if (operation.find(":\\") == 4) 
+			{
+				directory= operation.substr(3);
+			}
+			else
+			{
+				cout << "Uncorrect form!" << endl;
+			}
+		}
+		else if (operation.find("create") == 0) 
+		{
+			if (operation.find("create_null") == 0) 
+			{
+				Playlist A(operation.substr(12));
+			}
+			else
+			{
+				Playlist A(directory, operation.substr(7));
+				A.getSongs();
+			}
+		}
+		else if (operation.find("add") == 0)
+		{
+		}
+		else if (operation.find("minus") == 0)
+		{
+		}
+		else if (operation.find("exit") == 0)
+		{
+		}
+		else if (operation.find("help") == 0)
+		{
+			cout << "ls" << endl;
+			cout << "cd .." << endl;
+			cout << "cd subfolder\\" << endl;
+			cout << "cd full_adress\\" << endl;
+			cout << "create <PlaylistName.m3u>" << endl;
+			cout << "create_null <PlaylistName.m3u>" << endl;
 		}
 		else 
 		{
@@ -40,7 +97,7 @@ void ls(string directory)
 	hFind = FindFirstFile(directory.c_str(), &ffd);
 	if (INVALID_HANDLE_VALUE == hFind)
 	{
-		cout << "We couldn't find such file" << endl;
+		cout << "We couldn't find such directory" << endl;
 	}
 	else
 	{
